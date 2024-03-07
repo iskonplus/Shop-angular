ca3da4535e1bd6492d0ce2a8f9bf7071639e2430
iskonplus <58971909+iskonplus@users.noreply.github.com>
2024-03-07 18:34:54 +0100

    add footer appService & newProductService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { HttpClientModule } from "@angular/common/http";
 // import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { NavComponent } from "./components/nav/nav.component";
@@ -7,17 +8,31 @@ import { NewProductComponent } from "./components/new-product/new-product.compon
 import { BestsellerComponent } from "./components/bestseller/bestseller.component";
 import { ProductSaleComponent } from "./components/product-sale/product-sale.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { AppService } from './services/app.service';
 
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    standalone: true,
-    imports: [HeaderComponent, MatSlideToggleModule, NavComponent, NewProductComponent, BestsellerComponent, ProductSaleComponent, FooterComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [HeaderComponent, MatSlideToggleModule, NavComponent,
+    NewProductComponent, BestsellerComponent, ProductSaleComponent,
+    FooterComponent, HttpClientModule],
+  providers: [AppService]
 })
 
 export class AppComponent {
   title = 'Shop-angular';
+  url = "https://fakerapi.it/api/v1/products?_quantity=30&_taxes=1&_categories_type=uuid";
+
+  constructor(private appService: AppService) { }
+
+  ngOnInit(): void {
+    this.appService.getProducts(this.url).subscribe({
+      next: (data: any) => console.log(data)
+    });
+
+  }
 
 }
--- src/app/components/new-product/new-product.component.ts
+++ src/app/components/new-product/new-product.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
-
-import {MatCardModule} from '@angular/material/card'
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { MatCardModule } from '@angular/material/card';
 import { ProductCardsComponent } from "../product-cards/product-cards.component";
 import { ProductHeaderComponent } from "../product-header/product-header.component";
+import { NewProductService } from '../../services/new-product.service'
 
 @Component({
-    selector: 'app-new-product',
-    standalone: true,
-    templateUrl: './new-product.component.html',
-    styleUrl: './new-product.component.scss',
-    imports: [MatCardModule, ProductCardsComponent, ProductHeaderComponent]
+  selector: 'app-new-product',
+  standalone: true,
+  templateUrl: './new-product.component.html',
+  styleUrl: './new-product.component.scss',
+  imports: [MatCardModule, ProductCardsComponent, ProductHeaderComponent, HttpClientModule],
+  // providers: [NewProductService]
 })
-export class NewProductComponent {
-  title : string = 'New products';
+export class NewProductComponent implements OnInit {
+  title: string = 'New products';
+
+  constructor(private newProductService: NewProductService) { }
+
+  ngOnInit(): void {
+    this.newProductService.zzz();
+  }
 
 }
+
+
